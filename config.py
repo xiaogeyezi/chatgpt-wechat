2276768c8c1c3becad6e9a9783f38875089fac4a
xiaogeyezi <35679978+xiaogeyezi@users.noreply.github.com>
2023-03-30 10:45:26 +0000

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ available_setting = {
     # Bot触发配置
     "single_chat_prefix": ["bot", "@bot"],  # 私聊时文本需要包含该前缀才能触发机器人回复
     "single_chat_reply_prefix": [],  # 私聊时自动回复的前缀，用于区分真人
-    "group_chat_prefix": ["@bot"],  # 群聊时包含该前缀则会触发机器人回复
+    "group_chat_prefix": [],  # 群聊时包含该前缀则会触发机器人回复
     "group_chat_reply_prefix": "",  # 群聊时自动回复的前缀
     "group_chat_keyword": [],  # 群聊时包含该关键词则会触发机器人回复
     "group_at_off": False,  # 是否关闭群聊时@bot的触发
